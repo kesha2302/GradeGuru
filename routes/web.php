3ebe3756b3d237a8fa4ae88220575d77ac6d5c22
Kesha <keshapatel2302@gmail.com>
2025-06-23 13:25:06 +0530

    Trying to add kinjal routes

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminBannerController;
 use App\Http\Controllers\AdminClassPriceController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/Admin', [AdminPanelController::class, 'index']);
 Route::get('/Admin/Userdetail', [AdminPanelController::class, 'userdetail']);
@@ -64,3 +68,6 @@ Route::get('/Admin/Classprice/restore/{id}', [AdminClassPriceController::class, 
 Route::get('/Admin/RegularQuestions', [AdminClassNameController::class, 'index']);
 Route::get('/Admin/SuperQuestions', [AdminClassNameController::class, 'index']);
 Route::get('/Admin/Result', [AdminClassNameController::class, 'index']);
+
+
+
