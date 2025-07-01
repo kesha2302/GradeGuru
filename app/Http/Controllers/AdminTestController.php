da5c18b264aa5b9bf305a7689b3e4f03ed130f8d
Kesha <keshapatel2302@gmail.com>
2025-07-01 14:08:00 +0530

    testcontroller added

--- app/Http/Controllers/AdminTestController.php
+++ app/Http/Controllers/AdminTestController.php
@@ -9,5 +9,90 @@ use Illuminate\Support\Facades\Validator;
 
 class AdminTestController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $search = $request->input('search', '');
+        $query = Test::query();
+
+        if (!empty($search)) {
+            $query->where(function ($q) use ($search) {
+                $q->where('title', 'LIKE', "%$search%");
+                $q->orWhereHas('classPrice', function ($subQuery) use ($search) {
+                    $subQuery->where('title', 'LIKE', "%{$search}%");
+                });
+            });
+        }
+
+        $test = $query->paginate(10)->appends(['search' => $search]);
+
+        return view('AdminPanel.test', compact('test', 'search'));
+    }
+
+    public function testform()
+    {
+        $test = new Test;
+        $class_price = ClassPrice::all();
+        $url = url('/Admin/Testform2');
+        $title = "Test Detail Form";
+        $data = compact('url', 'title', 'class_price','test');
+
+        return view('AdminPanel.testform')->with($data);
+    }
+
+    public function testform2(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'class_price' => 'required|exists:class_prices,cp_id',
+            'title' => 'required|string',
+            'que_type' => 'required|string',
+
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $test = new Test();
+        $test->cp_id = $request->input('class_price');
+        $test->title = $request->input('title');
+        $test->que_type = $request->input('que_type');
+        $test->save();
+
+        return redirect('/Admin/Test');
+    }
+
+
+    public function testedit($id)
+    {
+        $test = Test::find($id);
+        if (is_null($test)) {
+            return redirect('/Admin/Tests');
+        } else {
+            $url = url('/Admin/Test/update') . "/" . $id;
+            $title = "Update Test Details";
+            $data = compact('test', 'url', 'title');
+            return view('AdminPanel.testform')->with($data);
+        }
+    }
+
+    public function testupdate($id, Request $request)
+    {
+        $test = Test::find($id);
+        $test->title = $request->input('title');
+        $test->que_type = $request->input('que_type');
+        $test->save();
+
+        return redirect('/Admin/Test');
+    }
+
+    public function testdelete($id)
+    {
+        $test = Test::find($id);
+        if (!is_null($test)) {
+            $test->delete();
+        }
+        return redirect('/Admin/Test');
+    }
+
 }
--- resources/views/ClientView/index.blade.php
+++ resources/views/ClientView/index.blade.php
@@ -1,35 +1,85 @@
 @extends('ClientView.Layouts.main')
 @section('main-section')
-<!-- resources/views/ClientView/index.blade.php -->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Home Page</title>
-</head>
-<body>
-    <div class="container-fluid bg-primary px-0 px-md-5 mb-5">
-      <div class="row align-items-center px-3">
-        <div class="col-lg-6 text-center text-lg-left">
-          <h4 class="text-white mb-4 mt-5 mt-lg-0">Kids Learning Center</h4>
-          <h1 class="display-3 font-weight-bold text-white">
-            New Approach to Kids Education
-          </h1>
-          <p class="text-white mb-4">
-            Sea ipsum kasd eirmod kasd magna, est sea et diam ipsum est amet sed
-            sit. Ipsum dolor no justo dolor et, lorem ut dolor erat dolore sed
-            ipsum at ipsum nonumy amet. Clita lorem dolore sed stet et est justo
-            dolore.
-          </p>
-          <a href="" class="btn btn-secondary mt-1 py-3 px-5">Learn More</a>
-        </div>
-        <div class="col-lg-6 text-center text-lg-right">
-          <img class="img-fluid mt-5" src="ClientView/img/header.png" alt="" />
-        </div>
-      </div>
-    </div>
+    <!-- resources/views/ClientView/index.blade.php -->
+    <!DOCTYPE html>
+    <html>
 
-    <h1>Welcome to GradeGuru!</h1>
-</body>
-</html>
+    <head>
+        <title>Home Page</title>
+    </head>
 
+    <body>
+        <div class="container-fluid bg-primary px-0 px-md-5 mb-5">
+            <div class="row align-items-center px-3">
+                <div class="col-lg-6 text-center text-lg-left">
+                    <h4 class="text-white mb-4 mt-5 mt-lg-0">Kids Learning Center</h4>
+                    <h1 class="display-3 font-weight-bold text-white">
+                        New Approach to Kids Education
+                    </h1>
+                    <p class="text-white mb-4">
+                        Sea ipsum kasd eirmod kasd magna, est sea et diam ipsum est amet sed
+                        sit. Ipsum dolor no justo dolor et, lorem ut dolor erat dolore sed
+                        ipsum at ipsum nonumy amet. Clita lorem dolore sed stet et est justo
+                        dolore.
+                    </p>
+                    <a href="" class="btn btn-secondary mt-1 py-3 px-5">Learn More</a>
+                </div>
+                <div class="col-lg-6 text-center text-lg-right">
+                    <img class="img-fluid mt-5" src="ClientView/img/header.png" alt="" />
+                </div>
+            </div>
+        </div>
+
+        <h1>Welcome to GradeGuru!</h1>
+
+
+
+        @php
+            $className = \App\Models\ClassName::all();
+        @endphp
+        <!-- Blog Start -->
+        <div class="container-fluid pt-5">
+            <div class="container">
+                <div class="text-center pb-2">
+                    <p class="section-title px-5">
+                        <span class="px-2">Classes</span>
+                    </p>
+                    <h1 class="mb-4">All Classes</h1>
+                </div>
+                <div class="row pb-3">
+                    @foreach ($className as $cn)
+                        <div class="col-lg-4 mb-4">
+                            <div class="card border-0 shadow-sm mb-2">
+                                <img class="card-img-top mb-2" src="{{ asset('ClientView/img/blog-1.jpg') }}"
+                                    alt="" />
+                                <div class="card-body bg-light text-center p-4">
+                                    <h3 class="">{{ $cn->standard }}</h3>
+                                    <h4 class="">{{ $cn->title }}</h4>
+                                    {{-- <div class="d-flex justify-content-center mb-3">
+                  <small class="mr-3"
+                    ><i class="fa fa-user text-primary"></i> Admin</small
+                  >
+                  <small class="mr-3"
+                    ><i class="fa fa-folder text-primary"></i> Web Design</small
+                  >
+                  <small class="mr-3"
+                    ><i class="fa fa-comments text-primary"></i> 15</small
+                  >
+                </div> --}}
+                                    <p>
+                                        {{ $cn->description }}
+                                    </p>
+                                    <a href="" class="btn btn-primary px-4 mx-auto my-2">View Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+
+                </div>
+            </div>
+        </div>
+        <!-- Blog End -->
+    </body>
+
+    </html>
 @endsection
